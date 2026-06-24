00ad98cc91d74fd6563a23982a0c4dd45464d5d6
Lev Dessyatykh <dessyatykh@gmail.com>
2026-06-24 20:39:35 +0500

    chore: remove react dependencies and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
   root: 'public',
@@ -8,7 +8,7 @@ export default defineConfig({
     emptyOutDir: true,
     minify: 'esbuild',
   },
-  plugins: [react()],
+
   test: {
     globals: true,
     environment: 'miniflare',
